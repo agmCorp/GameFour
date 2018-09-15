c07535871ce9926f5bec866787b3688d835d76f3
Alvaro G. Morales <alvaro.gabriel.morales@gmail.com>
2018-09-15 20:39:19 -0300

    Initial commit

---
+++ desktop/src/uy/com/agm/gamefour/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uy.com.agm.gamefour.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uy.com.agm.gamefour.GameFour;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameFour(), config);
+	}
+}
