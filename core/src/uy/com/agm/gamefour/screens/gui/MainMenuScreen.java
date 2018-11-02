2069c4315db81904b730d6311be1af00a31493d4
Alvaro G. Morales <alvaro.gabriel.morales@gmail.com>
2018-11-01 21:07:57 -0300

    No comments

--- core/src/uy/com/agm/gamefour/screens/gui/MainMenuScreen.java
+++ core/src/uy/com/agm/gamefour/screens/gui/MainMenuScreen.java
@@ -2,23 +2,24 @@ package uy.com.agm.gamefour.screens.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.I18NBundle;
 
 import uy.com.agm.gamefour.assets.Assets;
 import uy.com.agm.gamefour.assets.gui.AssetGUI;
 import uy.com.agm.gamefour.assets.sprites.AssetSprites;
-import uy.com.agm.gamefour.game.DebugConstants;
 import uy.com.agm.gamefour.game.GameFour;
 import uy.com.agm.gamefour.game.GameSettings;
 import uy.com.agm.gamefour.screens.ListenerHelper;
 import uy.com.agm.gamefour.screens.ScreenEnum;
 import uy.com.agm.gamefour.screens.ScreenTransitionEnum;
 import uy.com.agm.gamefour.screens.gui.widget.AnimatedActor;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveBy;
 
 /**
  * Created by AGMCORP on 17/9/2018.
@@ -27,18 +28,18 @@ import uy.com.agm.gamefour.screens.gui.widget.AnimatedActor;
 public class MainMenuScreen extends GUIAbstractScreen {
     private static final String TAG = MainMenuScreen.class.getName();
 
-    private static final float PAD = 30.0f;
-    private static final int BUTTON_COLUMNS = 3;
-    private static final float BUTTON_SIZE = 85.0f;
-    private static final float PLAY_BUTTON_SIZE = 155.0f;
     private static final float JUMPER_SCALE = 1.3f;
     private static final float ROCKET_BACKGROUND_SCALE = 0.2f;
+    private static final float TITLE_OFFSET_Y = 700.0f;
     private static final float ROCKET_BG_OFFSET_X = 200.0f;
     private static final float ROCKET_BG_OFFSET_Y = 50.0f;
     private static final float CLOUD_OFFSET_X = 40.0f;
     private static final float CLOUD_OFFSET_Y = 20.0f;
     private static final float JUMPER_OFFSET_Y = 200.0f;
-    private static final float ROCKET_FG_OFFSET_Y = 60;
+    private static final float ROCKET_FG_OFFSET_Y = 40.0f;
+    private static final float BUTTONS_OFFSET_Y = 350.0f;
+    private static final float BUTTONS_ANIM_DURATION = 2.0f;
+    private static final float BUTTONS_MOVE_BY_AMOUNT = 110.0f;
 
     private Assets assets;
     private AssetGUI assetGUI;
@@ -46,11 +47,16 @@ public class MainMenuScreen extends GUIAbstractScreen {
     private I18NBundle i18NGameThreeBundle;
     private GameSettings prefs;
     private Image menuBackground;
+    private Label gameTitle;
     private Image littleCloud;
     private Image trail;
     private AnimatedActor jumper;
     private AnimatedActor rocketBackground;
     private AnimatedActor rocketForeground;
+    private ImageButton play;
+    private ImageButton info;
+    private ImageButton audio;
+    private ImageButton exit;
 
     public MainMenuScreen(GameFour game) {
         super(game);
@@ -82,6 +88,12 @@ public class MainMenuScreen extends GUIAbstractScreen {
         menuBackground = new Image(assetGUI.getMenuBackground());
         stage.addActor(menuBackground);
 
+        // Title
+        Label.LabelStyle labelStyleGameTitle = new Label.LabelStyle();
+        labelStyleGameTitle.font = assets.getFonts().getDefaultGameTitle();
+        gameTitle = new Label(i18NGameThreeBundle.format("mainMenuScreen.gameTitle"), labelStyleGameTitle);
+        stage.addActor(gameTitle);
+
         // Distant rocket
         rocketBackground = new AnimatedActor(assetGUI.getRocket().getRocketAnimation(), ROCKET_BACKGROUND_SCALE);
         stage.addActor(rocketBackground);
@@ -102,46 +114,27 @@ public class MainMenuScreen extends GUIAbstractScreen {
         rocketForeground = new AnimatedActor(assetGUI.getRocket().getRocketAnimation());
         stage.addActor(rocketForeground);
 
-        // Main table (title and buttons)
-        stage.addActor(getMainTable());
+        // Buttons
+        defineButtons();
+        stage.addActor(audio);
+        stage.addActor(info);
+        stage.addActor(exit);
+        stage.addActor(play);
     }
 
-    private Table getMainTable() {
-        Table table = new Table();
-        table.setDebug(DebugConstants.DEBUG_LINES);
-        table.center();
-        table.setFillParent(true);
-        table.add(getTopTable()).height(GameFour.APPLICATION_HEIGHT / 2).row();
-        table.add(getBottomTable()).height(GameFour.APPLICATION_HEIGHT / 2);
-        return table;
-    }
-
-    private Table getTopTable() {
-        Label.LabelStyle labelStyleGameTitle = new Label.LabelStyle();
-        labelStyleGameTitle.font = assets.getFonts().getDefaultGameTitle();
-        Label gameTitle = new Label(i18NGameThreeBundle.format("mainMenuScreen.gameTitle"), labelStyleGameTitle);
-
-        Table table = new Table();
-        table.setDebug(DebugConstants.DEBUG_LINES);
-        table.top();
-        table.add(gameTitle);
-        table.padTop(PAD);
-        return table;
-    }
-
-    private Table getBottomTable() {
-        final ImageButton audio = new ImageButton(new TextureRegionDrawable(assetGUI.getAudio()),
+    private void defineButtons() {
+        audio = new ImageButton(new TextureRegionDrawable(assetGUI.getAudio()),
                 new TextureRegionDrawable(assetGUI.getAudioPressed()),
                 new TextureRegionDrawable(assetGUI.getAudioChecked()));
         audio.setChecked(!prefs.isAudio());
 
-        ImageButton play = new ImageButton(new TextureRegionDrawable(assetGUI.getPlay()),
+        play = new ImageButton(new TextureRegionDrawable(assetGUI.getPlay()),
                 new TextureRegionDrawable(assetGUI.getPlayPressed()));
 
-        ImageButton info = new ImageButton(new TextureRegionDrawable(assetGUI.getInfo()),
+        info = new ImageButton(new TextureRegionDrawable(assetGUI.getInfo()),
                 new TextureRegionDrawable(assetGUI.getInfoPressed()));
 
-        ImageButton exit = new ImageButton(new TextureRegionDrawable(assetGUI.getExit()),
+        exit = new ImageButton(new TextureRegionDrawable(assetGUI.getExit()),
                 new TextureRegionDrawable(assetGUI.getExitPressed()));
 
         // Events
@@ -160,15 +153,6 @@ public class MainMenuScreen extends GUIAbstractScreen {
                 Gdx.app.exit();
             }
         }));
-
-        Table table = new Table();
-        table.setDebug(DebugConstants.DEBUG_LINES);
-        table.bottom();
-        table.add(audio).width(BUTTON_SIZE);
-        table.add(play).size(PLAY_BUTTON_SIZE);
-        table.add(info).width(BUTTON_SIZE).row();
-        table.add(exit).colspan(BUTTON_COLUMNS).size(BUTTON_SIZE);
-        return table;
     }
 
     @Override
@@ -181,6 +165,9 @@ public class MainMenuScreen extends GUIAbstractScreen {
         // Place the menuBackground in the middle of the screen
         menuBackground.setX((w - menuBackground.getWidth()) / 2);
         menuBackground.setY((h - menuBackground.getHeight()) / 2);
+
+        // Place the title
+        gameTitle.setPosition((w - gameTitle.getWidth()) / 2, TITLE_OFFSET_Y);
 
         // Place a distant rocket
         rocketBackground.setX((w - rocketBackground.getWidth()) / 2 - ROCKET_BG_OFFSET_X);
@@ -201,5 +188,22 @@ public class MainMenuScreen extends GUIAbstractScreen {
         // Place a rocket in the foreground
         rocketForeground.setX((w - rocketForeground.getWidth()) / 2);
         rocketForeground.setY((h - rocketForeground.getHeight()) / 2 - ROCKET_FG_OFFSET_Y);
+
+        // Place buttons
+        play.setX((w - play.getWidth()) / 2);
+        play.setY((h - play.getHeight()) / 2 - BUTTONS_OFFSET_Y);
+        float x = play.getX() + play.getWidth() / 2 - audio.getWidth() / 2;
+        float y = play.getY() + play.getHeight() / 2 - audio.getHeight() / 2;
+        audio.setPosition(x, y);
+        info.setPosition(x, y);
+        exit.setPosition(x, y);
+
+        // Set actions
+        audio.clearActions();
+        audio.addAction(moveBy(-BUTTONS_MOVE_BY_AMOUNT, BUTTONS_MOVE_BY_AMOUNT, BUTTONS_ANIM_DURATION, Interpolation.smooth));
+        play.clearActions();
+        play.addAction(moveBy(0, BUTTONS_MOVE_BY_AMOUNT, BUTTONS_ANIM_DURATION, Interpolation.smooth));
+        info.clearActions();
+        info.addAction(moveBy(BUTTONS_MOVE_BY_AMOUNT, BUTTONS_MOVE_BY_AMOUNT, BUTTONS_ANIM_DURATION, Interpolation.smooth));
     }
 }
